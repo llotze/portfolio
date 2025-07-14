58ea807dabe59bd0a469367b7435108a9358c979
llotze <llotze@bu.edu>
2025-07-14 02:41:54 -0400

    fixed pathing for fonts after deployment

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-const repo = 'portfolio'; // your repo name
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-};
+  // Remove the basePath and assetPrefix - let GitHub Pages handle this automatically
+  basePath: '',
+  assetPrefix: '',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
